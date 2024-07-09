b4d236e7d75ac2bb88a721fe14167d05ba6d2f41
teej-hub <timothylee.0902@gmail.com>
2024-07-09 13:27:51 -0700

    Added all cardData properly, i think

--- scripts/index.js
+++ scripts/index.js
@@ -49,8 +49,9 @@ function getCardElement(cardData){
     const cardElement = cardTemplate.cloneNode(true);
     const cardImageEl = cardElement.querySelector('.card__image');
     const cardTitleEl = cardElement.querySelector('.card__title');
-    cardTitleEl.textContent = cardData.title;
-    
+    cardTitleEl.textContent = cardData.name;
+    cardImageEl.src = cardData.link;
+    cardImageEl.alt = cardData.name;
     return cardElement;
 }
 
