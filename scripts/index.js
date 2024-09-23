44aa65dd5b9b4254b2711d72b5c3825508af75e8
teej-hub <timothylee.0902@gmail.com>
2024-09-23 15:59:17 -0700

    "Figured out why profile form wasn't linked to its save button"

--- scripts/index.js
+++ scripts/index.js
@@ -27,32 +27,48 @@ const initialCards = [
 /*--------------------------------------------------------------------*/
 /*                            Elements                                */
 /*--------------------------------------------------------------------*/
+// Buttons 
 const editButton = document.querySelector("#profile-edit-button");
+const closeProfile = document.querySelector("#profile-close-button");
+const saveProfile = document.querySelector("#modal-input"); // Linked saveProfile to the form instead of the button, and the submit listener works now
+const closeCard = document.querySelector("#card-create-close-button");
+const saveCard = document.querySelector("#card-save-button");
+
+// popup
 const profileModal = document.querySelector("#profile-edit-modal");
-const closeProfileModal = document.querySelector("#profile-close-button");
-const saveProfile = document.querySelector("#profile-save-button");
+const profileEditForm = profileModal.querySelector("#modal-input");
+const cardModal = document.querySelector("#card-create-modal");
+
+// data, input
 const profileName = document.querySelector(".profile__name");
 const profileDesc = document.querySelector(".profile__subtitle");
 const inputProfileName = document.querySelector("#modal-input-name");
 const inputProfileDesc = document.querySelector("#modal-input-text");
-const profileEditForm = profileModal.querySelector("#modal-input");
-const cardListEl = document.querySelector('.cards__list');
-const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
-const cardModal = document.querySelector("#card-create-modal");
-const closeCardModal = document.querySelector("#card-create-close-button");
 const inputCardTitle = document.querySelector("#modal-input-title");
 const inputCardURL = document.querySelector("#modal-input-url");
-const saveCard = document.querySelector("#card-save-button");
+
+
+// cards
+const cardListEl = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+
+
 /*--------------------------------------------------------------------*/
 /*                            Functions                               */
 /*--------------------------------------------------------------------*/
 
 
-function closeProfile(){
-    profileModal.classList.remove("modal_opened");
-}
-function closeCard(){
-    cardModal.classList.remove("modal_opened");
+function closeForm(element){
+    var toClose;
+    if(element == "profile"){
+        toClose = profileModal;
+    }
+    else{
+        toClose = cardModal;
+    }
+    
+    console.log("called closeForm");
+    toClose.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData){
@@ -62,6 +78,7 @@ function getCardElement(cardData){
     cardTitleEl.textContent = cardData.name;
     cardImageEl.src = cardData.link;
     cardImageEl.alt = cardData.name;
+    console.log("getCardElement called");
     return cardElement;
 }
 
@@ -84,32 +101,42 @@ function handleProfileFormSubmit (e){
     console.log("handle");
     profileName.textContent = inputProfileName.value;
     profileDesc.textContent = inputProfileDesc.value;
-    closeProfile();
+    console.log("inputProfileName.value(): " + inputProfileName.value);
+    closeForm("profile");
 }
+
 function handleCardFormSubmit (e){
     e.preventDefault();
     cardTitleEl.textContent = inputCardTitle.value;
     cardImageEl.textContent = inputCardURL.value;
-    closeCard();
+    closeForm("card");
+}
+
+
+function testSave (e){
+    e.preventDefault();
+    profileName.textContent = inputProfileName.value;
+    profileDesc.textContent = inputProfileDesc.value;
+    console.log("saveProfile eventlistener triggered");
+    closeForm("profile");
 }
 
 /*--------------------------------------------------------------------*/
 /*                          Event Listeners                           */
 /*--------------------------------------------------------------------*/
+
 editButton.addEventListener("click", () => {
     console.log("event listener");
-    /*inputProfileName.value = profileName.textContent;
-    inputProfileDesc.value = profileDesc.textContent;*/
     profileModal.classList.add("modal_opened");
 });
 
-closeProfileModal.addEventListener("click", closeProfile);
+
+closeProfileModal.addEventListener("click", closeForm("profile"));
 saveProfile.addEventListener("submit", handleProfileFormSubmit);
-closeCardModal.addEventListener("click", closeCard);
-saveCard.addEventListener("submit", handleProfileFormSubmit);
+closeCardModal.addEventListener("click", closeForm("profile"));
+saveCard.addEventListener("submit", handleCardFormSubmit);
 
 initialCards.forEach((cardData) => {
     const cardElement = getCardElement(cardData);
     cardListEl.append(cardElement);
 })
-//making new commit, testing mac 
